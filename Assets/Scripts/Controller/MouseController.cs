d3300f930c08c1b1f65f316df2defe60fb46d71c
Ravenholdt <joppe4899@hotmail.com>
2016-08-08 04:08:12 +0200

    Förflyttning med mus
    Det går nu att förflytta kameran med musen.

---
+++ Assets/Scripts/Controller/MouseController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+
+	Vector3 lastFramePosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 currFramePosition = Camera.main.ScreenToWorldPoint ( Input.mousePosition );
+
+		if (Input.GetMouseButton (1)) { //Right Mouse Button
+
+			Vector3 diff = lastFramePosition - currFramePosition;
+			Camera.main.transform.Translate (diff);
+
+		}
+
+		lastFramePosition = Camera.main.ScreenToWorldPoint ( Input.mousePosition );
+
+	}
+}
